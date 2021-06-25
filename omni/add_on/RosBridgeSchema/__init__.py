026f671548c6c9b64304e4be3e3f7821b91e35bd
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2021-06-25 09:39:23 +0000

    Add development notice in imports

--- omni/add_on/RosBridgeSchema/__init__.py
+++ omni/add_on/RosBridgeSchema/__init__.py
@@ -3,6 +3,7 @@ from pxr import Tf
 try:
     from . import _rosBridgeSchema
 except:
+    print(">>>> [DEVELOPMENT] import rosBridgeSchema")
     from . import rosBridgeSchema as _rosBridgeSchema
 
 Tf.PrepareModule(_rosBridgeSchema, locals())
--- omni/add_on/RosControlBridgeSchema/__init__.py
+++ omni/add_on/RosControlBridgeSchema/__init__.py
@@ -3,6 +3,7 @@ from pxr import Tf
 try:
     from . import _rosControlBridgeSchema
 except:
+    print(">>>> [DEVELOPMENT] import rosControlBridgeSchema")
     from . import rosControlBridgeSchema as _rosControlBridgeSchema
 
 Tf.PrepareModule(_rosControlBridgeSchema, locals())
