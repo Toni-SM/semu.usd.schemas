4a7d00c35fecb06ca8bd03e35bc808ab64c16154
Antonio Serrano Muñoz <toni.sm91@gmail.com>
2021-10-23 17:39:01 +0000

    Add ROSAttribute schema

--- omni/add_on/RosBridgeSchema/rosBridgeSchema.py
+++ omni/add_on/RosBridgeSchema/rosBridgeSchema.py
@@ -78,3 +78,45 @@ class RosCompressedCamera(ROSSchema.RosBridgeComponent):
         if includeInherited: 
             names = ROSSchema.RosBridgeComponent.GetSchemaAttributeNames(includeInherited)
         return names + ["resolution", "rgbPubTopic", "depthPubTopic", "frameId", "rgbEnabled", "depthEnabled", "queueSize"]
+
+
+class RosAttribute(ROSSchema.RosBridgeComponent):
+    def __init__(self, prim):
+        # RosBridgeComponent:
+        # __init__(_object*, pxrInternal_v0_20__pxrReserved__::UsdSchemaBase schemaObj)
+        # __init__(_object*, pxrInternal_v0_20__pxrReserved__::UsdPrim prim)
+        # __init__(_object*)
+        super().__init__(prim)
+        
+    def __bool__(self):
+        return self.GetPrim().IsDefined()
+
+    @staticmethod
+    def Define(stage, path):
+        prim = stage.DefinePrim(path, "RosAttribute")
+        return RosAttribute(prim)
+    
+    def CreateSetterSrvTopicAttr(self, value):
+        self.GetPrim().CreateAttribute("setterSrvTopic", Sdf.ValueTypeNames.String, True).Set(value)
+
+    def CreateGetterSrvTopicAttr(self, value):
+        self.GetPrim().CreateAttribute("getterSrvTopic", Sdf.ValueTypeNames.String, True).Set(value)
+
+    @staticmethod
+    def Get(stage, path):
+        # Get(pxrInternal_v0_19__pxrReserved__::TfWeakPtr<pxrInternal_v0_19__pxrReserved__::UsdStage> stage, pxrInternal_v0_19__pxrReserved__::SdfPath path)
+        prim = stage.GetPrimAtPath(path)
+        return RosAttribute(prim)
+
+    def GetSetterSrvTopicAttr(self):
+        return self.GetPrim().GetAttribute("setterSrvTopic")
+
+    def GetGetterSrvTopicAttr(self):
+        return self.GetPrim().GetAttribute("getterSrvTopic")
+
+    @staticmethod
+    def GetSchemaAttributeNames(includeInherited=True):
+        names = []
+        if includeInherited: 
+            names = ROSSchema.RosBridgeComponent.GetSchemaAttributeNames(includeInherited)
+        return names + ["setterSrvTopic", "getterSrvTopic"]
